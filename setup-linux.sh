cc2483a2838a4f32abf648fd3129c7f92a179675
Ale Bark <ale.bark_bruneri@hotmail.com>
2020-05-28 22:20:41 -0300

    adding deb downloads

--- setup-linux.sh
+++ setup-linux.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-#Tratando possíveis erros
+#Disabling erros from stopping the script
 set -e
 
+#Some folders that i like to remove/create
 mkdir $HOME/Softwares
+mkdir $HOME/Projects
+
 rm -r $HOME/Public $HOME/Templates
 
 dir_downloads=$HOME/Downloads
 dir_softwares=$HOME/Softwares
+dir_projects=$HOME/Projects
 
 arr_snaps=(postman)
 arr_snaps_classic=(slack skype)
@@ -19,27 +23,30 @@ arr_downloads=(
 
 cd $dir_softwares
 
-echo 'Atualizando pacotes...'
+echo 'Packages Update...this may take a while'
 sudo apt update
 sudo apt upgrade -y
 
-# Iniciando download dos pacotes para setup de desenvolvimento
-echo 'Instalando Nginx'
+# Starting to download development softwares packages
+echo 'Nginx installation'
 sudo apt install -y nginx
 
-echo 'Instalando PHP 7.4'
+echo 'PHP 7.4 installation'
 sudo add-apt-repository ppa:ondrej/php -y -u
 sudo apt update
 sudo apt-get install -y --no-install-recommends php7.4 php7.4-{bcmath,bz2,intl,gd,mbstring,mysql,zip,fpm}
 
-echo 'Instalando NVM'
+echo 'NVM installation'
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 nvm install node -y
 nvm install-latest-npm
 
-echo 'Instalando PostgreSQL'
+echo 'PostgreSQL installation'
 sudo apt install -y postgresql postgresql-contrib
 
-echo 'Instalando SNAPS'
+echo 'Snaps installation'
 snap install ${arr_snaps[@]}
 snap install --classic ${arr_snaps_classic[@]}
+
+
+wget -nv -c ${downloads[@]}
